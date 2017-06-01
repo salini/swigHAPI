cc40570cf723385c676bc11a6dfb4890f82a4124
jsa <jsa@spaceapplications.com>
2017-06-01 16:08:17 +0200

    fix unity packaging script

--- src/unity/createUnitySources.py
+++ src/unity/createUnitySources.py
@@ -7,6 +7,7 @@ import glob
 def copyFiles(src, dest, extension):
     files = glob.iglob(os.path.join(src, "*."+extension))
     for file in files:
+        #print file
         if os.path.isfile(file):
             shutil.copy2(file, dest)
 
@@ -19,15 +20,22 @@ thisFilePath, buildDir, csHAPIDir, currentSrcDir = sys.argv
 #create unity folder
 unitySrcDir = os.path.join(currentSrcDir, "unity")
 unityBinDir = os.path.join(buildDir     , "unity")
+unitycsHAPIScriptDir = os.path.join(unityBinDir, "csHAPI", "Scripts")
+unityPluginsDir      = os.path.join(unityBinDir, "Plugins")
+
+#print "unitySrcDir", unitySrcDir
+#print "unityBinDir", unityBinDir
+#print "unitycsHAPIScriptDir", unitycsHAPIScriptDir
+#print "unityPluginsDir", unityPluginsDir
+
 if os.path.exists(unityBinDir):
     shutil.rmtree(unityBinDir)
 
 shutil.copytree(unitySrcDir, unityBinDir)
+os.makedirs(unitycsHAPIScriptDir)
+os.makedirs(unityPluginsDir)
 
-unitycsHAPIScrpitDir = os.path.join(unityBinDir, "csHAPI", "Scripts")
-unityPluginsDir      = os.path.join(unityBinDir, "Plugins")
-
-copyFiles(csHAPIDir, unitycsHAPIScrpitDir, "cs")
+copyFiles(csHAPIDir, unitycsHAPIScriptDir, "cs")
 copyFiles(csHAPIDir, unityPluginsDir, "dll")
 
 
